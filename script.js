299736938ba458c150d8f8f0eee7376639b4f3a8
Octupus13 <107840787+Octupus13@users.noreply.github.com>
2023-08-21 00:28:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// script.js
+const bucketUrl = 'https://nkm-01130113.s3.amazonaws.com/'
+
+const filesPath = fetchFilePath();
+
+const cardData = filesPath.map(filePath =>{
+    return  { imageUrl: bucketUrl+filePath.URL, title: filePath.TITLE, content: filePath.CONTENT }
+})
+  
+  const cardContainer = document.getElementById("cardContainer");
+  
+  for (const cardInfo of cardData) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+  
+    const imageElement = document.createElement("img");
+    imageElement.src = cardInfo.imageUrl;
+    imageElement.alt = cardInfo.title;
+  
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = cardInfo.title;
+  
+    const contentElement = document.createElement("p");
+    contentElement.textContent = cardInfo.content;
+  
+    cardElement.appendChild(imageElement);
+    cardElement.appendChild(titleElement);
+    cardElement.appendChild(contentElement);
+  
+    cardContainer.appendChild(cardElement);
+  }
+
+
+  async function fetchFilePath(){
+    try{
+
+        const response = await fetch('http://34.196.65.142:3000')
+        
+        if(!response.ok){
+            throw new Error("fetxh บ่ได่")
+        }
+        
+        const filesPath = await response.json();
+        return filesPath
+    }catch(err){
+        console.error("Error :", err)
+        return [];
+    }
+  }
+  
